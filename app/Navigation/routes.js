46716ca595893e1531674640ff7867d158d098d7
Deepak Govindarajan <deepak.g@krds.fr>
2019-09-16 10:12:23 +0530

    current location fetcher

--- app/Navigation/routes.js
+++ app/Navigation/routes.js
@@ -1,6 +1,6 @@
 import HomeScreen from '../Views/Home';
 import AboutScreen from '../Views/About';
-
+import CurrentLocationScreen from '../Views/CurrentLocation';
 
 /* Icon key is optional. It must be of type string and its value should match a valid provider icon
   name.
@@ -9,6 +9,7 @@ import AboutScreen from '../Views/About';
 export default [
   { name: 'Home', screen: HomeScreen, icon: 'book' },
   { name: 'About', screen: AboutScreen, icon: 'information-outline' },
+  { name: 'Current Location', screen: CurrentLocationScreen, icon: 'map' }
 ];
 // note whenever searching for icon - look in MaterialCommunityIcons section in below link
 // https://oblador.github.io/react-native-vector-icons/
